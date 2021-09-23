2bc643708cd7544b15609bccd58a47ee45063cdb
Alexander Pushkarev <alexspush@gmail.com>
2021-09-23 20:53:22 +0300

    Fixing configuration

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import infrastructure.discord.discord_adapter
+
+from flask import Flask
+
+app = Flask(__name__)
+
+pinged = 0
+
+@app.route('/ping')
+def ping():
+    pinged += 1
+    return f'Pong! (pinged {pinged} times)'
