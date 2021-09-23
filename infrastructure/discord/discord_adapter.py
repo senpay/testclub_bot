84042d980be81b3167ecf947e7513419014b2208
Alexander Pushkarev <alexspush@gmail.com>
2021-09-23 20:47:28 +0300

    Add heroku configuration

---
+++ infrastructure/discord/discord_adapter.py
@@ -0,0 +1,22 @@
+import discord
+from just_config.configuration import Configuration
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+config = Configuration()
+
+client.run(config['TOKEN'])
---
+++ main.py
@@ -0,0 +1,1 @@
+import infrastructure.discord.discord_adapter
