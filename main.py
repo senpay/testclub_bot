b0c0857bd7444992c1b04daad95dd81b0d299b46
Alexander Pushkarev <alexspush@gmail.com>
2021-09-23 21:00:17 +0300

    fix flask app

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 pinged = 0
 
+
 @app.route('/ping')
 def ping():
+    global pinged
     pinged += 1
     return f'Pong! (pinged {pinged} times)'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
