6e9a76eb0c113af8bb2e9347e119cca00d96e314
Divaon <56230474+Divaon@users.noreply.github.com>
2021-09-27 13:32:17 +0300

    FisrtIteration

--- infrastructure/discord/discord_adapter.py
+++ infrastructure/discord/discord_adapter.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 from just_config.configuration import Configuration
 
 client = discord.Client()
@@ -16,6 +17,12 @@ async def on_message(message):
 
     if message.content.startswith('$bot'):
         await message.channel.send('Шо нада была, хазяин?!')
+
+@commands.Cog.listener()
+async def on_member_join(self, member):
+    channel = member.guild.system_channel
+    await channel.send(embed=discord.Embed(description=f'{member.mention} явился на службу.'))
+
 
 config = Configuration()
 
