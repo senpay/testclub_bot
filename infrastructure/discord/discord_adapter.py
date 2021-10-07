671efb22cdbbd73708b0218081b751f15bcc3e88
Divaon <56230474+Divaon@users.noreply.github.com>
2021-10-07 22:16:34 +0300

    Json
    Json

--- infrastructure/discord/discord_adapter.py
+++ infrastructure/discord/discord_adapter.py
@@ -1,9 +1,16 @@
 import discord
 import logging
+<<<<<<< Updated upstream
 from discord.ext import commands
+=======
+>>>>>>> Stashed changes
 from just_config.configuration import Configuration
+import json
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 intents=discord.Intents.default()
 intents.members=True
 client = discord.Client(intents=intents)
@@ -15,14 +22,27 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
+<<<<<<< Updated upstream
     channel=member.guild.system_channel
     await channel.send(f'Владыка {member.mention} явился!')
+=======
+    with open ('Hellomessage.json','r') as f:
+        message=json.load(f)
+    channel=member.guild.system_channel
+    await channel.send(f'{message["1"]}{member.mention} {message["2"]}')
+>>>>>>> Stashed changes
 
 @client.event
 async def on_member_remove(member):
     channel=member.guild.system_channel
     logging.info(f'{member.mention} exit')
+<<<<<<< Updated upstream
     await channel.send(f'Владыка {member.mention} предал нас!')
+=======
+    with open ('Traitormessage.json','r') as f:
+        message=json.load(f)
+    await channel.send(f'{message["1"]}{member.mention} {message["2"]}')
+>>>>>>> Stashed changes
 
 @client.event
 async def on_message(message):
@@ -30,7 +50,10 @@ async def on_message(message):
         return
 
     if message.content.startswith('$bot'):
+<<<<<<< Updated upstream
         print('see')
+=======
+>>>>>>> Stashed changes
         await message.channel.send('Rо нада была, хазяин?!')
 
 
