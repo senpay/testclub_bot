4a0d7f3445d2fa818458a4157d3efaefd427a106
Divaon <56230474+Divaon@users.noreply.github.com>
2021-10-25 22:32:08 +0300

    Welcome and farewell message (#5)
    Add configurable welcome and farewell message
    
    Co-authored-by: Alex Pushkarev <alexspush@gmail.com>

--- infrastructure/discord/discord_adapter.py
+++ infrastructure/discord/discord_adapter.py
@@ -1,8 +1,13 @@
 import discord
+import logging
+from discord.ext import commands
+
 from just_config.configuration import Configuration
+import json
 
-client = discord.Client()
-
+intents=discord.Intents.default()
+intents.members=True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -10,12 +15,30 @@ async def on_ready():
 
 
 @client.event
+async def on_member_join(member):
+    with open ('Hellomessage.json','r') as f:
+        message=json.load(f)
+    channel=member.guild.system_channel
+    await channel.send(f'{message["1"]}{member.mention} {message["2"]}')
+
+@client.event
+async def on_member_remove(member):
+    channel=member.guild.system_channel
+    logging.info(f'{member.mention} exit')
+    with open ('Traitormessage.json','r') as f:
+        message=json.load(f)
+    await channel.send(f'{message["1"]}{member.mention} {message["2"]}')
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$bot'):
-        await message.channel.send('Шо нада была, хазяин?!')
+        await message.channel.send('Rо нада была, хазяин?!')
+
+
+
 
 config = Configuration()
 
