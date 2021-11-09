b2a837450d6619f0c01823c0bc3d783d615d7a9a
Divaon <56230474+Divaon@users.noreply.github.com>
2021-11-09 23:15:48 +0300

    Database BadMaseeges
    1

--- infrastructure/discord/discord_adapter.py
+++ infrastructure/discord/discord_adapter.py
@@ -3,19 +3,27 @@ from discord.ext import commands
 import logging
 from just_config.configuration import Configuration
 import json
+import repositories
+from repositories import UserRepositoryDis, PostRepositoryDis
+import string
+import os,sqlite3
 
+user_repository=UserRepositoryDis()
+post_repository=PostRepositoryDis()
 intents=discord.Intents.default()
 intents.members=True
-client = discord.Client(intents=intents)
-client=commands.Bot(command_prefix='$')
+# cl = discord.Client(intents=intents)
+client=commands.Bot(command_prefix='$', intents=intents)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
+
 @client.event
 async def on_member_join(member):
+    print('Catch event join')
     with open ('DiscordBotMessage.json','r') as f:
         message=json.load(f)
     channel=member.guild.system_channel
@@ -26,29 +34,38 @@ async def on_member_join(member):
 
 @client.event
 async def on_member_remove(member):
-    with open ('DiscordBotMessage.json','r') as f:
-        message=json.load(f)
+    print('Catch event exit')
+    with open('DiscordBotMessage.json', 'r') as f:
+        message = json.load(f)
     channel = member.guild.system_channel
-    logging.info(f'{member.mention} exit')
-    traitor_message=message["traitor_message"]
-    traitor_message=traitor_message.replace('${username}',member.mention )
+    logging.info(f'{member.mention} come')
+    traitor_message = message["traitor_message"]
+    traitor_message = traitor_message.replace('${username}', member.mention)
     await channel.send(traitor_message)
 
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def kick(ctx,member: discord.Member,*,reason=None):
-    print("Hi")
     await ctx.send(f'Попытка кикнуть')
     await member.kick(reason=reason)
     await ctx.send(f'Succes')
     await ctx.send(f'Пользователя {member.mention} выгнали.')
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#     if message.content.startswith('$bot'):
-#         await message.channel.send('Rо нада была, хазяин?!')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if 'mag' in message.content.lower():
+        if user_repository.get_user(message.author.mention)==None:
+            userid=user_repository.add_user(message.author.mention)
+        else:
+            user=user_repository.get_user(message.author.mention)
+            userid=user.id
+        post_repository.add_post(message.content,userid)
+        await message.channel.send('Bard word.')
+    if message.content.startswith('$bot'):
+        await message.channel.send('Rо нада была, хазяин?!')
+    await client.process_commands(message)
 
 config = Configuration()
 
---
+++ readme.txt
@@ -0,0 +1,3 @@
+export GOOGLE_APPLICATION_CREDENTIALS=/home/divaon/PycharmProjects/bot/gcloud.json
+gcloud auth login
+gcloud config set project brilliant-forge-317117
